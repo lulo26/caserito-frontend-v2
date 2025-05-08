50d7d789ad80cff6ebe3a313812edb97a6124a1e
lulo <lopez.gallego2611@gmail.com>
2025-05-08 08:47:02 -0500

    modals

--- src/views/productos/components/ModalNuevo.jsx
+++ src/views/productos/components/ModalNuevo.jsx
@@ -29,7 +29,6 @@ export default function modalProducto() {
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-        
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
