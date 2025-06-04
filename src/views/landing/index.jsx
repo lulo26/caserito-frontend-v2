a832b4ac8829171c7fa4ec54269e3cedceb9131a
Sebas <sebaslg96@gmail.com>
2025-06-04 00:39:52 -0500

    culminada la landing

--- src/views/landing/index.jsx
+++ src/views/landing/index.jsx
@@ -5,16 +5,27 @@ import 'react-multi-carousel/lib/styles.css';
 import Button from '@mui/material/Button';
 import { Link, useNavigate } from 'react-router';
 
+import { Parallax, Background } from 'react-parallax';
 
 import CircularProgress from '@mui/material/CircularProgress';
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import { Grid2, Card, Box, CardActions, CardContent, CardMedia } from "@mui/material";
+import { Grid2, Card, Box, CardActions, CardContent, CardMedia, Stack } from "@mui/material";
 import Typography from '@mui/material/Typography';
 
 import { ACCESS_TOKEN_NAME, baseURL } from '../../store/constant';
-import { Parallax } from 'react-parallax';
+import f1 from './assets/img/f1.png'
+import f2 from './assets/img/f2.png'
+import f3 from './assets/img/f3.png'
+import bg4 from './assets/img/bg4.png'
+import bg5 from './assets/img/bg5.png'
+import bg6 from './assets/img/bg6.png'
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import IconButton from '@mui/material/IconButton';
 
 export default function Landing() {
 
@@ -51,26 +62,10 @@ const imgURL = 'http://168.231.112.194:8098/storage/'
     const navigate = useNavigate()
 
     useEffect(() => {
-        // Make GET request to fetch data
-/*         axios.get(productoURL)
-            .then((response) => {
-                setData(response.data);
-                setLoading(false);
-            })
-            .catch((err) => {
-                setError(err.message);
-                setLoading(false);
-            }); */
             axios({
                 method: 'get',
-                url: '/producto',
-                baseURL: baseURL,
+                url: productoURL,
                 responseType: 'json',
-                proxy: {
-                    protocol: 'http',
-                    host: '168.231.112.194',
-                    port: 8098,
-                },
             })
             .then((response) => {
                 setData(response.data);
@@ -108,20 +103,36 @@ const imgURL = 'http://168.231.112.194:8098/storage/'
 
   return (
 <>
-    <div class="container main-container">
-        <nav>
-            <Link to={loginLink}>{loginText}</Link>
-        </nav>
-        <div class="bottomText">
-            <a href="#productos">Ver Más</a>
-        </div>
-    </div>
-      
-      <div class="blank" id='productos'>
-        <span class="blankText">Nuestros productos</span>
-      </div>
-<br /><br />
 
+<Parallax blur={0} bgImage={f1} bgImageAlt="the cat" strength={300}>
+        <div class="headerSection" id='main'>
+        <span class="blankText">
+            <ul>
+                <li><a href='#productos'>Productos</a></li>
+                <li><a href='#contacto'>Contacto</a></li>
+                <li><a href='#nosotros'>Nosotros</a></li>
+                <li><Link to={loginLink}>{loginText}</Link></li>
+            </ul>
+        </span>
+    </div>
+    <div class="container">
+        <Parallax blur={0} bgImage={f2} bgImageAlt="the cat" strength={200}>
+            <div class="container">
+                <Parallax blur={0} bgImage={f3} bgImageAlt="the cat" strength={0}>
+                    <div class="container">
+                    </div>
+                </Parallax>
+            </div>
+        </Parallax>
+    </div>
+</Parallax>
+<Parallax blur={{min:0,max:10}} bgImage={bg5} bgImageAlt="the cat" strength={200}>
+
+    <div class="headerSection" id='productos'>
+      <span class="blankText">Nuestros productos</span>
+    </div>
+
+    <div class="container">
 <Box sx={{margin:"2rem"}}>
     <Carousel
     swipeable={true}
@@ -168,20 +179,47 @@ const imgURL = 'http://168.231.112.194:8098/storage/'
 </Box>
 
 
-     <br /> 
-    <div class="second">
     </div>
-      
-    <div class="blank" id='nosotros'>
+</Parallax>
+
+<Parallax blur={{min:0,max:15}} bgImage={bg6} bgImageAlt="the cat" strength={300}>
+    <div class="headerSection" id='contacto'>
+    <span class="blankText">¡Contactanos!</span>
+    </div>
+    <div class="container contacto">
+        <Stack direction='row' spacing={5}>
+            <IconButton><FacebookIcon  sx={{fontSize:100}} /></IconButton>
+            <IconButton><InstagramIcon sx={{fontSize:100}}  /></IconButton>
+            <IconButton><WhatsAppIcon  sx={{fontSize:100}} /></IconButton>
+        </Stack>
+        <br />
+        <Typography variant='h2'>Telefono de contacto: +57 000 000 000</Typography>
+        <Typography variant='h2'>Estamos ubicados en la carrera ejemplo con calle test</Typography>
+    </div>
+</Parallax>
+
+<Parallax blur={{min:0,max:10}} bgImage={bg4} bgImageAlt="the cat" strength={300}>
+    <div class="headerSection" id='nosotros'>
         <span class="blankText">Sobre Nosotros</span>
     </div>
-    <div class="about">
-        <Box>
-            <Typography variant="h2">
-                Somos una repostería especializada en la elaboración de diferentes tipos de postres, ubicada en la zona céntrica de la ciudad de Cartago, con calidad y presentación artesanal. 
-            </Typography>
-        </Box>
+    <div class="container">
+        <div class="about">
+            <Box>
+                <Typography variant="h2">
+                MISIÓN: La repostería Caserito Postres tiene como objetivo satisfacer las necesidades de las personas que quieran darse un gustico, además ofrecerles una dulce experiencia de sabores con nuestros increíbles postres caseritos a base de leche.
+                </Typography>
+                <br /><br />
+                <Typography variant="h2">
+                VISIÓN: Nuestro objetivo a largo plazo (2029) es innovar nuestro portafolio de productos ofreciendo postres personalizados y de temporada, llegar a ser una empresa sólida financieramente, ser reconocidos a nivel municipal, incrementar nuestras ventas y llegar a más clientes. 
+                </Typography>
+            </Box>
+        </div>
     </div>
+</Parallax>
+
+
+      
+
 </>
 );
 }
