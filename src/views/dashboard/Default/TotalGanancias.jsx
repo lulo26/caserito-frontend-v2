57bc40ff22765027cf871ba20c2302a614a24501
lulo <lopez.gallego2611@gmail.com>
2025-06-04 01:15:38 -0500

    charts

---
+++ src/views/dashboard/Default/TotalGanancias.jsx
@@ -0,0 +1,160 @@
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid2';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+import SkeletonEarningCard from 'ui-component/cards/Skeleton/EarningCard';
+
+// assets
+import EarningIcon from 'assets/images/icons/earning.svg';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import GetAppTwoToneIcon from '@mui/icons-material/GetAppOutlined';
+import FileCopyTwoToneIcon from '@mui/icons-material/FileCopyOutlined';
+import PictureAsPdfTwoToneIcon from '@mui/icons-material/PictureAsPdfOutlined';
+import ArchiveTwoToneIcon from '@mui/icons-material/ArchiveOutlined';
+
+import axios from 'axios';
+import { baseURL } from '../../../store/constant';
+
+export default function TotalGanancias({ isLoading }) {
+  let ventas;
+  const theme = useTheme();
+  const [ventasTotal, setVentasTotal] = useState()
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(()=>{
+    try {
+      axios.get(`${baseURL}/venta`)
+      .then((response)=>{
+        ventas = (response.data.data)
+        let totalSuma = 0
+      for (let i = 0; i < ventas.length; i++) {
+        totalSuma += parseInt(ventas[i].recibido)
+        
+      }
+      setVentasTotal(totalSuma)
+      }) 
+    }
+      catch (error) {
+        console.log(`error ${error}`);
+      }
+      
+  },[])
+
+  
+
+  return (
+    <>
+      {isLoading ? (
+        <SkeletonEarningCard />
+      ) : (
+        <MainCard
+          border={false}
+          content={false}
+          aria-hidden={Boolean(anchorEl)}
+          sx={{
+            bgcolor: 'primary.main',
+            color: '#fff',
+            overflow: 'hidden',
+            position: 'relative',
+            '&:after': {
+              content: '""',
+              position: 'absolute',
+              width: 210,
+              height: 210,
+              background: theme.palette.primary[200],
+              borderRadius: '50%',
+              top: { xs: -85 },
+              right: { xs: -95 }
+            },
+            '&:before': {
+              content: '""',
+              position: 'absolute',
+              width: 210,
+              height: 210,
+              background: theme.palette.primary[200],
+              borderRadius: '50%',
+              top: { xs: -125 },
+              right: { xs: -15 },
+              opacity: 0.5
+            }
+          }}
+        >
+          <Box sx={{ p: 2.25 }}>
+            <Grid container direction="column">
+              <Grid>
+                <Grid container sx={{ justifyContent: 'space-between' }}>
+                  <Grid>
+                    <Avatar
+                      variant="rounded"
+                      sx={{
+                        ...theme.typography.commonAvatar,
+                        ...theme.typography.largeAvatar,
+                        bgcolor: 'secondary.dark',
+                        mt: 1
+                      }}
+                    >
+                      <CardMedia sx={{ width: 24, height: 24 }} component="img" src={EarningIcon} alt="Notification" />
+                    </Avatar>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid>
+                <Grid container sx={{ alignItems: 'center' }}>
+                  <Grid>
+                    <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>${ventasTotal}</Typography>
+                  </Grid>
+                  <Grid>
+                    <Avatar
+                      sx={{
+                        cursor: 'pointer',
+                        ...theme.typography.smallAvatar,
+                        bgcolor: 'primary.main',
+                        color: 'primary.dark'
+                      }}
+                    >
+                      <ArrowUpwardIcon fontSize="inherit" sx={{ transform: 'rotate3d(1, 1, 1, 45deg)' }} />
+                    </Avatar>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid sx={{ mb: 1.25 }}>
+                <Typography
+                  sx={{
+                    fontSize: '1rem',
+                    fontWeight: 500,
+                    color: 'orange.200'
+                  }}
+                >
+                  Total Recibido
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </MainCard>
+      )}
+    </>
+  );
+}
+
+TotalGanancias.propTypes = { isLoading: PropTypes.bool };
