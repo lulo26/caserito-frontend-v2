6a5c849584999aa47015dad2edac8950bce5d58b
Sebas <sebaslg96@gmail.com>
2025-06-13 16:45:16 -0500

    modificando rutas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 
 // routes
 import AuthenticationRoutes from './AuthenticationRoutes';
+import LandingRoutes from './LandingRoutes';
 import { MainRoutes } from './MainRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
-const router = createBrowserRouter([MainRoutes, AuthenticationRoutes], {
+const router = createBrowserRouter([LandingRoutes, MainRoutes, AuthenticationRoutes], {
   basename: import.meta.env.VITE_APP_BASE_NAME
 });
 
